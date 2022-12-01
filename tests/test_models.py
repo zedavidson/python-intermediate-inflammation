33e4af6255be5272bd7ef21eda844dac70ecd1a1
Zoe Dvaidson <zoe.davidson1@nhs.net>
2022-12-01 14:48:13 +0000

    Progress with tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -40,6 +40,28 @@ def test_daily_max():
 
     npt.assert_array_equal(daily_max(test_input), test_result)
 
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0,0], [0,0], [0,0]], [0,0]),
+        ([[1,2], [3,4], [5,6]], [3,4]),
+    ]
+)
+def test_daily_mean(test, expected):
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+        ([[4, 2, 5], [1, 6, 2], [4, 1, 9]], [4, 6, 9]),
+    ]
+)
+def test_daily_max(test, expected):
+    from inflammation.models import daily_max
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
+
 def test_daily_min_string():
     from inflammation.models import daily_min
 
